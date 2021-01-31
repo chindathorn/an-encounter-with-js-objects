1ba8fd30afde3c95a2e3cb381bd5936d926b2ee1
chinda <53336246+chindathorn@users.noreply.github.com>
2021-01-31 17:09:49 +0700

    Update array-methods.js

--- array-methods.js
+++ array-methods.js
@@ -68,3 +68,47 @@ const items = [1, 2, 3, 4, 5]
 const includesTwo = items.includes(8)
 
 console.log(includesTwo)
+
+const vehicle = { car: "🚗", truck: "🚚", bike: "🚲" };
+
+for (const key in vehicle) {
+  console.log(vehicle[key]);
+}
+
+for (const v of Object.values(vehicle)) {
+  console.log(v);
+}
+
+// const vehicleArr = new Map({ car: "🚗", truck: "🚚", bike: "🚲" });
+const vehicleArr = new Map(
+  Object.entries({ car: "🚗", truck: "🚚", bike: "🚲" })
+);
+
+for (const v of vehicleArr.values()) {
+  console.log(v);
+}
+
+var moons = ["🌝", "🌑", "🌒", "🌓", "🌔", "🌑"];
+
+// create a new array with that only that value
+var moon = moons.filter(m => m === "🌒");
+
+var cleaned = moons.map(m => "❄");
+
+// check it contain something
+// if any element is return true then the some() is return true
+const hasFullLight = moon.some(m => m === "🌙");
+
+// ensure that every array follow some pattern
+const isEmoji = moons.every(m => m > "y");
+
+// check unwant item in array
+const darkCount = moons.reduce((acc, cur) => {
+  return acc + (cur === "🌑" ? 1 : 0);
+}, 0 /* start index */);
+
+// sort
+const sorted = moons.sort((a, b) => a < b);
+sorted.splice(4);
+
+console.log(sorted);
